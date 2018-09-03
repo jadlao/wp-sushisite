cdbf480834f85226d4269036423770558d4cf591
Josef Adlao <jadlao@gmail.com>
2018-09-03 21:07:29 +1000

    first commit

---
+++ index.php
@@ -0,0 +1,152 @@
+<html>
+
+<head>
+  <title>Sushi</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="<?php echo get_bloginfo('template_directory'); ?>/css/style.css">
+</head>
+
+<body>
+  <div>
+    <div class="sidebar-brand">
+      <div class="social">
+        <a href="#">
+          <i class="fa fa-facebook"></i>
+        </a>
+        <a href="#">
+          <i class="fa fa-twitter"></i>
+        </a>
+        <a href="#">
+          <i class="fa fa-instagram"></i>
+        </a>
+      </div>
+      <div class="brand">
+        <h1 class="rotate">SushiSite</h1>
+      </div>
+    </div>
+    <section class="home">
+      <div class="left-content">
+        <nav>
+          <ul>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Gallery</a>
+            </li>
+            <li>
+              <a href="#">Menu</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </nav>
+        <h2>HEADING GOES HERE</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+          dolore magna
+          aliqua.
+        </p>
+        <a href="#" id="btn-book-top">Book Now</a>
+        <div class="box-next">
+          <a href="#">See our menu
+            <i class="fa fa-arrow-down" aria-hidden="true"></i>
+          </a>
+        </div>
+      </div>
+      <div class="right-content">
+      </div>
+    </section>
+    <section class="about">
+      <div class="left-content">
+        <h2>HEADING GOES HERE</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+          dolore magna
+          aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat.
+        </p>
+      </div>
+      <div class="right-content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+          dolore magna
+          aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat.
+        </p>
+      </div>
+    </section>
+    <section class="gallery">
+      <div class="left-img">
+      </div>
+      <div class="gallery-carousel">
+        <img src="http://yestoyolks.com/wp-content/uploads/2014/08/IMG_2713.jpg" class="img-1 circle">
+        <p>Ramen noodles</p>
+        <img src="http://yestoyolks.com/wp-content/uploads/2014/08/IMG_2713.jpg" class="img-2 circle">
+        <p>Ramen noodles</p>
+        <img src="http://yestoyolks.com/wp-content/uploads/2014/08/IMG_2713.jpg" class="img-3 circle">
+        <p>Ramen noodles</p>
+      </div>
+    </section>
+    <section class="menu">
+      <h2>Menu</h2>
+      <div class="left-menu">
+        <h3>Sushi</h3>
+        <h4>Sashimi</h4>
+        <p>5 Salmon sashimi, wasabi, soy sauce</p>
+        <h4>Sashimi</h4>
+        <p>5 Salmon sashimi, wasabi, soy sauce</p>
+        <h4>Sashimi</h4>
+        <p>5 Salmon sashimi, wasabi, soy sauce</p>
+        <h4>Sashimi</h4>
+        <p>5 Salmon sashimi, wasabi, soy sauce</p>
+      </div>
+      <div class="right-menu">
+        <h3>A La Carte</h3>
+        <h4>Sashimi</h4>
+        <p>5 Salmon sashimi, wasabi, soy sauce</p>
+        <h4>Sashimi</h4>
+        <p>5 Salmon sashimi, wasabi, soy sauce</p>
+        <h4>Sashimi</h4>
+        <p>5 Salmon sashimi, wasabi, soy sauce</p>
+        <h4>Sashimi</h4>
+        <p>5 Salmon sashimi, wasabi, soy sauce</p>
+      </div>
+      <button>Download Menu</button>
+    </section>
+    <section class="contact">
+      <h2>Contact</h2>
+      <div class="location">
+        <i class="fa fa-location-arrow" aria-hidden="true"></i>
+        <h3>Location</h3>
+        <p>Level 5, Shangri-La, 80 Grand Ave</p>
+        <p>Sydney NSW 2000</p>
+      </div>
+      <div class="hours">
+        <i class="fa fa-clock-o" aria-hidden="true"></i>
+        <h3>Opening hours</h3>
+        <p>Monday: 9am - 11pm</p>
+        <p>Tuesday: 9am - 11pm</p>
+        <p>Wednesday: 9am - 11pm</p>
+        <p>Thursday: 9am - 11pm</p>
+        <p>Friday: 9am - 11pm</p>
+        <p>Saturday: 9am - 11pm</p>
+        <p>Sunday: 9am - 11pm</p>
+      </div>
+    </section>
+    <section class="reservations">
+      <h2>Book Now</h2>
+      <form>
+        <input type="text" placeholder="How many people is this booking for?" />
+        <br>
+        <input type="text" placeholder="When would you like to dine?" />
+        <br>
+        <br>Calendar goes here
+        <br>
+        <br>
+        <button>Book</button>
+      </form>
+    </section>
+  </div>
+</body>
+
+</html>
